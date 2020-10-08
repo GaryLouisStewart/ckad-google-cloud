d88ec17d8bc0eb7134f703f6ad3ad8a308ef72db
Gary Stewart <gary.stewart@example.com>
2020-10-08 23:25:48 +0100

    Fix ssh-key generation, permissions update.

--- vm-images/kube/scripts/copy_ssh_keys.sh
+++ vm-images/kube/scripts/copy_ssh_keys.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
-# copy over the public key into authorised ssh keys for this image.
+# copy over the public key into authorised ssh keys for this image
+# set correct permissions on the directory.
 
-sudo bash -c "cat /${HOME}/.ssh/kube.pub >> /${HOME}/.ssh/authorized_keys"
-chmod 400 /${HOME}/.ssh/kube
+
+commands=(
+    "chmod 700 ${HOME}/.ssh"
+    "chmod 600 ${HOME}/.ssh/authorized_keys"
+    "chmod 400 ${HOME}/.ssh/kube"
+)
+
+# create authorized_keys file in .ssh directory.
+cat "${HOME}"/.ssh/kube.pub >> "${HOME}"/.ssh/authorized_keys
+
+# loop over commands array variable and execute each string as a command.
+for i in "${commands[@]}"; do
+    $i
+done
